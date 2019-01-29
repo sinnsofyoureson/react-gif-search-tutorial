b2d3f6efe7b91d169e8fd12b0ab52f6cbd0c440e
Rinat <sinnsofvalkyrie@gmail.com>
2019-01-29 17:52:09 +0400

    Updates

---
+++ precache-manifest.6b68e8034e4cabb525546ca4755d7701.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "be67f41bd440f99efe01",
+    "url": "/react-gif-search-tutorial/static/css/main.c27ca9eb.chunk.css"
+  },
+  {
+    "revision": "be67f41bd440f99efe01",
+    "url": "/react-gif-search-tutorial/static/js/main.be67f41b.chunk.js"
+  },
+  {
+    "revision": "cd40acc8d3e07c14d028",
+    "url": "/react-gif-search-tutorial/static/js/1.cd40acc8.chunk.js"
+  },
+  {
+    "revision": "2716297b8b6a8114b6c2",
+    "url": "/react-gif-search-tutorial/static/js/runtime~main.2716297b.js"
+  },
+  {
+    "revision": "3b1ad5fd266f88c346a5dea4ecd504c5",
+    "url": "/react-gif-search-tutorial/index.html"
+  }
+];
